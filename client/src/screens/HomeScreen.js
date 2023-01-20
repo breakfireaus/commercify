fd0cead0051b2a0ae99b520fa1daf39754c1f805
breakfireaus <myoungerc@hotmail.com>
2023-01-21 08:53:44 +1100

    started working on backend

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('api/products')
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest products</h1>

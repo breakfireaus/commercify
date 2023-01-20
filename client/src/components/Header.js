453ca22a7bd003d1ab5f2d96075c02d7d6a836f9
breakfireaus <myoungerc@hotmail.com>
2023-01-20 21:18:19 +1100

    routing for page so will be single page app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,30 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='light' variant='light' expand='lg'>
+      <Navbar bg='light' variant='light' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Commercify</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Commercify</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link className='ml-auto' href='/cart'>
-                <i className='fas fa-shopping-cart'></i>
-                Cart
-              </Nav.Link>
-              <Nav.Link className='ml-auto' href='/login'>
-                <i className='fas fa-user'></i>
-                Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>

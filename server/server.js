c75bb1e4c6bded2bb2fd62e4d133056866a6d82f
breakfireaus <myoungerc@hotmail.com>
2023-01-25 23:09:10 +1100

    fixed mongo issue and updated package.json

--- server/server.js
+++ server/server.js
@@ -18,5 +18,6 @@ app.get('/api/product/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, console.log(`Server running on port ${PORT}`))

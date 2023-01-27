4056a556b62253b2feb9710b96b54f12b1822805
breakfireaus <myoungerc@hotmail.com>
2023-01-27 14:37:53 +1100

    added model for users

---
+++ server/models/users.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    UserPassword: {
+      type: String,
+      required: true,
+    },
+    userAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User

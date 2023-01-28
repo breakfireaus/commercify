257c940df38840dc0feafdff90a0430209e399c4
breakfireaus <myoungerc@hotmail.com>
2023-01-29 07:34:27 +1100

    added product reducer

--- client/store.js
+++ client/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension'
+import { configureStore } from '@reduxjs/toolkit'
+import { productListReducer } from './src/reducers/productReducer'
 
-const reducer = combineReducers
+const reducer = combineReducers({
+  productList: productListReducer,
+})
 const initialstate = {}
 const middleware = [thunk]
-const store = createStore(
+const store = configureStore(
   reducer,
   initialstate,
   composeWithDevTools(applyMiddleware(...middleware))

272416efdbaaab80f87f6f834f73e68d52e2b4e4
breakfireaus <myoungerc@hotmail.com>
2023-01-29 19:20:41 +1100

    fixed a bug

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useParams } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
-const CartScreen = ({ match, location, history }) => {
-  const productId = match.params.id
-
-  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+const CartScreen = () => {
+  const { id } = useParams()
+  const { search } = useLocation()
+  const productId = id
+  const qty = search ? Number(search.split('=')[1]) : 1
 
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)

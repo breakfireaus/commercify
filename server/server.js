a494ebc49f410383cbc506cb59f6585c5734e320
breakfireaus <myoungerc@hotmail.com>
2023-02-05 22:35:46 +1100

    deploying

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import morgan from 'morgan'
 
 dotenv.config()
 
@@ -16,16 +17,27 @@ app.use(express.json())
 
 app.use('/api/product', productRoutes)
 app.use('/api/users', userRoutes)
-app.use(notFound)
 
-app.use(errorHandler)
+const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+app.use(notFound)
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
